e97d19dd938d0344014f5d23f4434d8024d83689
83544844@qq.com <83544844@qq.com>
2023-06-16 18:32:39 +0800

    学大数据，上多易教育

--- rt_rule_marketing/src/main/java/cn/doitedu/demo9/model_meta.sql
+++ rt_rule_marketing/src/main/java/cn/doitedu/demo9/model_meta.sql
@@ -10,22 +10,17 @@ Target Server Type    : MYSQL
 Target Server Version : 50738
 File Encoding         : 65001
 
-Date: 2023-06-16 18:31:27
+Date: 2023-06-16 18:31:18
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for rule_meta
+-- Table structure for model_meta
 -- ----------------------------
-DROP TABLE IF EXISTS `rule_meta`;
-CREATE TABLE `rule_meta` (
-  `rule_id` varchar(255) NOT NULL,
-  `rule_model_id` varchar(255) DEFAULT NULL,
-  `rule_param_json` text,
-  `online_status` int(11) DEFAULT NULL,
-  `pre_select_crowd` mediumblob,
-  `dynamic_profile_history_end_time` bigint(20) DEFAULT NULL,
+DROP TABLE IF EXISTS `model_meta`;
+CREATE TABLE `model_meta` (
+  `model_id` varchar(11) NOT NULL,
   `calculator_code` mediumtext,
-  PRIMARY KEY (`rule_id`)
+  PRIMARY KEY (`model_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
